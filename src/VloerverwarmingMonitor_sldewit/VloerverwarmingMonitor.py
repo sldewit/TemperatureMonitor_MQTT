56b849811be6e0a4d69ef71bb01dac538ad74ca3
sldewit <54032526+sldewit@users.noreply.github.com>
2023-01-20 16:04:51 +0100

    Removed some of the lint errors

--- src/VloerverwarmingMonitor_sldewit/VloerverwarmingMonitor.py
+++ src/VloerverwarmingMonitor_sldewit/VloerverwarmingMonitor.py
@@ -1,25 +1,26 @@
-import paho.mqtt.client as mqtt
 from threading import Thread, Event
+import paho.mqtt.client as mqtt
 from pi1wire import Pi1Wire, Resolution
 
-class TemperatureSensor:
+class temperature_sensor:
+    """Class representing a temperature sensor"""
     topic = ""
     temperature = 0.0
     sensorstate = "offline"
     sensoraddress = 0
     temperaturesensor = 0
-    MQTT_Broker = 0
+    mqtt_broker = 0
 
     def __init__(self, topic, sensoraddress, mqtt_broker) -> None:
         self.topic = topic
-        self.MQTT_Broker = mqtt_broker
+        self.mqtt_broker = mqtt_broker
         self.sensoraddress = sensoraddress
         try:
             self.temperaturesensor = Pi1Wire().find(self.sensoraddress)
             self.sensorstate = "online"
         except Exception as e:
             self.sensorstate = "not found"
-            print(f"Exception initializing sensor: {e}")    
+            print(f"Exception initializing sensor: {e}")
 
     def read(self):
         if self.temperaturesensor != 0:
@@ -28,15 +29,21 @@ class TemperatureSensor:
                 self.sensorstate = "online"
             except Exception as e:
                 self.sensorstate = "offline"
-                print(f"Exception while reading sensor: {e}")  
+                print(f"Exception while reading sensor: {e}")
         else: #simulate in case not really there
-            self.temperature += 0.1            
+            self.temperature += 0.1
             print(f"Simulate sensor: {self.topic} - {self.temperature}")
 
     def publish(self):
         try:
-            self.MQTT_Broker.publish(self.topic,payload = '{"temperatuur":"%.1f"}'%self.temperature , qos=0, retain=True)
-            self.MQTT_Broker.publish(self.topic+'/attributes', payload = '{"mac_address":"'+self.sensoraddress+'","status":"'+self.sensorstate+'"}', qos=0, retain=False)
+            self.mqtt_broker.publish(self.topic,
+                                     payload = '{"temperatuur":"%.1f"}'%self.temperature,
+                                     qos=0, retain=True)
+            self.mqtt_broker.publish(self.topic+'/attributes',
+                                     payload = '{"mac_address":"'+self.sensoraddress+'",
+                                     "status":"'+self.sensorstate+'"}',
+                                     qos=0,
+                                     retain=False)
         except Exception as e:
             print(f"MQTT publish failed: {e}")
 
@@ -47,6 +54,7 @@ def on_connect(client, userdata, flags, rc):
         print(f"Connected fail with code {rc}")
 
 class MyThread(Thread):
+    """Class for time thread"""
     interval = 1
     def __init__(self, event, interval):
         Thread.__init__(self)
@@ -61,7 +69,7 @@ class MyThread(Thread):
                 sensor.publish()
 
 try:
-    client = mqtt.Client() 
+    client = mqtt.Client()
     client.on_connect = on_connect
     client.will_set('vloerverwarming/status', "offline")
     client.username_pw_set("mqtt","test_mqtt")
@@ -76,11 +84,11 @@ except Exception as e:
 
 Sensors = []
 
-Sensors.append(TemperatureSensor('vloerverwarming/kring1/aanvoertemp',"28dfc6571f64ff",client))
-Sensors.append(TemperatureSensor('vloerverwarming/kring1/afvoertemp',"28dfd9571f64ff",client))
-Sensors.append(TemperatureSensor('vloerverwarming/kring2/aanvoertemp',"2828ff571f64ff",client))
-Sensors.append(TemperatureSensor('vloerverwarming/kring2/afvoertemp',"28aafd571f64ff",client))
-Sensors.append(TemperatureSensor('vloerverwarming/aanvoertemp',"282bfe571f64ff",client))
+Sensors.append(temperature_sensor('vloerverwarming/kring1/aanvoertemp',"28dfc6571f64ff",client))
+Sensors.append(temperature_sensor('vloerverwarming/kring1/afvoertemp',"28dfd9571f64ff",client))
+Sensors.append(temperature_sensor('vloerverwarming/kring2/aanvoertemp',"2828ff571f64ff",client))
+Sensors.append(temperature_sensor('vloerverwarming/kring2/afvoertemp',"28aafd571f64ff",client))
+Sensors.append(temperature_sensor('vloerverwarming/aanvoertemp',"282bfe571f64ff",client))
 
 stopFlag = Event()
 thread= MyThread(stopFlag, 10)
